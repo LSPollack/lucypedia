3852fd00b9bc9f1664d46e17c3bbb06c0f057c32
Lisa Pollack <lisa.s.pollack@gmail.com>
2015-02-15 22:04:22 +0000

    uses alternative implementation for google omniauth gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,33 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  def self.from_omniauth(auth)
-    if user = User.find_by_email(auth.info.email)
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user
-    else
-      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-        user.provider = auth.provider
-        user.uid = auth.uid
-        user.email = auth.info.email
-        user.password = Devise.friendly_token[0,20]
+  # def self.from_omniauth(auth)
+  #   if user = User.find_by_email(auth.info.email)
+  #     user.provider = auth.provider
+  #     user.uid = auth.uid
+  #     user
+  #   else
+  #     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+  #       user.provider = auth.provider
+  #       user.uid = auth.uid
+  #       user.email = auth.info.email
+  #       user.password = Devise.friendly_token[0,20]
+  #     end
+  #   end
+  # end
+
+  def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+      data = access_token.info
+      user = User.where(:email => data["email"]).first
+
+      # Uncomment the section below if you want users to be created if they don't exist
+      unless user
+          user = User.create(name: data["name"],
+             email: data["email"],
+             password: Devise.friendly_token[0,20]
+          )
       end
-    end
+      user
   end
 
   def role?(role_to_compare)
